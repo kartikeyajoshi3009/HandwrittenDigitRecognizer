51406dac694a3174bac7875c65580d72ab825333
Kartikeya Joshi <91309849+kartikeyajoshi3009@users.noreply.github.com>
2024-07-12 18:22:48 +0530

    Update digitrecognizer.py

--- digitrecognizer.py
+++ digitrecognizer.py
@@ -6,34 +6,30 @@ from PIL import Image, ImageOps
 # Load the trained model
 model = load_model(r'C:\Users\karti\OneDrive\Desktop\mini project\handwritten_digit_rec.h5')
 
-# Function to preprocess the input image
+# Pre-process
 def preprocess_image(img):
-    img = img.convert('L')  # Convert to grayscale
-    img = ImageOps.invert(img)  # Invert colors: white background to black
-    img = img.resize((28, 28))  # Resize to 28x28 pixels
-    img = np.array(img)  # Convert to array
-    img = img / 255.0  # Normalize to [0, 1] range
-    img = np.expand_dims(img, axis=0)  # Expand dimensions to match model input
-    img = np.expand_dims(img, axis=-1)  # Add channel dimension
+    img = img.convert('L')
+    img = ImageOps.invert(img)
+    img = img.resize((28, 28))
+    img = np.array(img)
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=-1) 
     return img
 
-# Function to predict the digit
+# Predict the digit
 def predict_digit(img):
     img = preprocess_image(img)
     predictions = model.predict(img)
     predicted_class = np.argmax(predictions, axis=1)
     return predicted_class[0]
 
-# Streamlit UI
+#UI
 st.title('Handwritten Digit Recognition')
-
-uploaded_file = st.file_uploader("Choose an image...", type="png")
+uploaded_file = st.file_uploader("Choose an image", type="png")
 
 if uploaded_file is not None:
-    # Load and display the uploaded image
     img = Image.open(uploaded_file)
     st.image(img, caption='Uploaded Image', use_column_width=True)
-
-    # Predict the digit
     predicted_class = predict_digit(img)
     st.write(f'Predicted Class: {predicted_class}')
